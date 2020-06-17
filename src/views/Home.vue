613a508a1c303181fb3a581da12ac5f5e338e429
huangjiakai <80004009@sfmail.sf-express.com>
2020-06-17 18:47:45 +0800

    hotfix 修复

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>我是供练习 git flow 的项目</div>
+  <div>
+    <i v-text="name"></i>
+    <div>我是供练习 git flow 的项目</div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      name: 'i am home'
+    }
+  }
 }
 </script>
